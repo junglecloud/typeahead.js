6fe0f5e18bb0c864af5405168492802d92f21052
Gabriel Conceição <gabriel_7340@hotmail.com>
2013-11-27 17:23:50 +0000

    transport.js: Add handler to call function on remote typeahead

--- src/typeahead.js
+++ src/typeahead.js
@@ -4,86 +4,144 @@
  * Copyright 2013 Twitter, Inc. and other contributors; Licensed MIT
  */
 
-(function() {
-  var cache = {}, viewKey = 'ttView', methods;
+var Transport = (function() {
+  var pendingRequestsCount = 0,
+      pendingRequests = {},
+      maxPendingRequests,
+      requestCache;
 
-  methods = {
-    initialize: function(datasetDefs) {
-      var datasets;
+  function Transport(o) {
+    utils.bindAll(this);
 
-      datasetDefs = utils.isArray(datasetDefs) ? datasetDefs : [datasetDefs];
+    o = utils.isString(o) ? { url: o } : o;
 
-      if (datasetDefs.length === 0) {
-        $.error('no datasets provided');
+    requestCache = requestCache || new RequestCache();
+
+    // shared between all instances, last instance to set it wins
+    maxPendingRequests = utils.isNumber(o.maxParallelRequests) ?
+      o.maxParallelRequests : maxPendingRequests || 6;
+
+    this.url = o.url;
+    this.wildcard = o.wildcard || '%QUERY';
+    this.filter = o.filter;
+    this.replace = o.replace;
+
+    this.ajaxSettings = {
+      type: 'get',
+      cache: o.cache,
+      timeout: o.timeout,
+      dataType: o.dataType || 'json',
+      beforeSend: o.beforeSend
+    };
+
+    this._get = (/^throttle$/i.test(o.rateLimitFn) ?
+      utils.throttle : utils.debounce)(this._get, o.rateLimitWait || 300);
+  }
+
+  utils.mixin(Transport.prototype, {
+
+    // private methods
+    // ---------------
+
+    _get: function(url, cb) {
+      var that = this;
+
+      // Verify if have an handler in 'o'bject structure and call function with wildcard, cb
+       if(typeof o.handler == 'function'){ 
+        o.handler(wildcard,cb); 
+        return; 
       }
 
-      datasets = utils.map(datasetDefs, function(o) {
-        var dataset = cache[o.name] ? cache[o.name] :  new Dataset(o);
+      // under the pending request threshold, so fire off a request
+      if (belowPendingRequestsThreshold()) {
+        this._sendRequest(url).done(done);
+      }
 
-        if (o.name) {
-          cache[o.name] = dataset;
-        }
+      // at the pending request threshold, so hang out in the on deck circle
+      else {
+        this.onDeckRequestArgs = [].slice.call(arguments, 0);
+      }
 
-        return dataset;
-      });
+      // success callback
+      function done(resp) {
+        var data = that.filter ? that.filter(resp) : resp;
 
-      return this.each(initialize);
+        cb && cb(data);
 
-      function initialize() {
-        var $input = $(this),
-            deferreds,
-            eventBus = new EventBus({ el: $input });
-
-        deferreds = utils.map(datasets, function(dataset) {
-          return dataset.initialize();
-        });
-
-        $input.data(viewKey, new TypeaheadView({
-          input: $input,
-          eventBus: eventBus = new EventBus({ el: $input }),
-          datasets: datasets
-        }));
-
-        $.when.apply($, deferreds)
-        .always(function() {
-          // deferring to make it possible to attach a listener
-          // for typeahead:initialized after calling jQuery#typeahead
-          utils.defer(function() { eventBus.trigger('initialized'); });
-        });
+        // cache the resp and not the results of applying filter
+        // in case multiple datasets use the same url and
+        // have different filters
+        requestCache.set(url, resp);
       }
     },
 
-    destroy: function() {
-      return this.each(destroy);
+    _sendRequest: function(url) {
+      var that = this, jqXhr = pendingRequests[url];
 
-      function destroy() {
-        var $this = $(this), view = $this.data(viewKey);
+      if (!jqXhr) {
+        incrementPendingRequests();
+        jqXhr = pendingRequests[url] =
+          $.ajax(url, this.ajaxSettings).always(always);
+      }
 
-        if (view) {
-          view.destroy();
-          $this.removeData(viewKey);
+      return jqXhr;
+
+      function always() {
+        decrementPendingRequests();
+        pendingRequests[url] = null;
+
+        // ensures request is always made for the last query
+        if (that.onDeckRequestArgs) {
+          that._get.apply(that, that.onDeckRequestArgs);
+          that.onDeckRequestArgs = null;
         }
       }
     },
 
-    setQuery: function(query) {
-      return this.each(setQuery);
+    // public methods
+    // --------------
 
-      function setQuery() {
-        var view = $(this).data(viewKey);
+    get: function(query, cb) {
+      var that = this,
+          encodedQuery = encodeURIComponent(query || ''),
+          url,
+          resp;
 
-        view && view.setQuery(query);
+      cb = cb || utils.noop;
+
+      url = this.replace ?
+        this.replace(this.url, encodedQuery) :
+        this.url.replace(this.wildcard, encodedQuery);
+
+      // in-memory cache hit
+      if (resp = requestCache.get(url)) {
+        // defer to stay consistent with behavior of ajax call
+        utils.defer(function() { cb(that.filter ? that.filter(resp) : resp); });
       }
-    }
-  };
 
-  jQuery.fn.typeahead = function(method) {
-    if (methods[method]) {
-      return methods[method].apply(this, [].slice.call(arguments, 1));
-    }
+      else {
+        this._get(url, cb);
+      }
 
-    else {
-      return methods.initialize.apply(this, arguments);
+      // return bool indicating whether or not a cache hit occurred
+      return !!resp;
     }
-  };
+  });
+
+  return Transport;
+
+  // static methods
+  // --------------
+
+  function incrementPendingRequests() {
+    pendingRequestsCount++;
+  }
+
+  function decrementPendingRequests() {
+    pendingRequestsCount--;
+  }
+
+  function belowPendingRequestsThreshold() {
+    return pendingRequestsCount < maxPendingRequests;
+  }
 })();
